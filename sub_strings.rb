7fff62598afb328ebccdf0468dff9d92f30ee0b6
Grace <graceminix@gmail.com>
2022-05-19 16:43:38 +0200

    Spliced string into array.

---
+++ sub_strings.rb
@@ -0,0 +1,8 @@
+my_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+    array = string.chars
+    
+end
+
+substrings("below", my_dictionary)
